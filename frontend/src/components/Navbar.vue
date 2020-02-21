b34ae353f2ccdd3dd2a7e39bb19d062f9322d06e
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-21 22:36:23 +0530

    create Navbar.vue

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    nav.green
+      .nav-wrapper.container
+        router-link(to="/").brand-logo Logo
+        ul.right.hide-on-med-and-down
+          li
+            router-link(to="/dashboard") Dashboard
+</template>
+
+<script>
+export default {
+	name: 'Navbar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
