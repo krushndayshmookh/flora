68979ecae05c523f919a7a00736af07c15970ae6
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-21 22:35:04 +0530

    add navbar and autoinit M

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,20 @@
-<template>
-  <router-view />
+<template lang="pug">
+  #app
+    Navbar
+    router-view
 </template>
+
+<script>
+import M from 'materialize-css'
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+	name: 'App',
+	components: {
+		Navbar
+	},
+	mounted() {
+		M.AutoInit()
+	}
+}
+</script>
