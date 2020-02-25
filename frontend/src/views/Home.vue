4fcde8386c0129f61e6745a942a9420d9583020a
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-25 15:14:40 +0530

    main mnu

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,6 +6,33 @@
           .col.s12
             h3 Home
 
+            p TODO: implement a role wise menu for farmer govt consumer expert doctor
+
+        .row
+          .col.s12
+            h4.light Farmer
+          
+          .col.s3
+            .card-panel.green Check Fields
+          
+          .col.s3
+            .card-panel.yellow Sell produce
+          
+          .col.s3
+            .card-panel.orange Buy tools
+          
+          .col.s3
+            .card-panel.pink Disease identifier
+
+        .row
+          .col.s12
+            h4.light Consumer
+          
+          .col.s3
+            .card-panel.teal Buy vegetables
+          
+
+
         
 </template>
 
