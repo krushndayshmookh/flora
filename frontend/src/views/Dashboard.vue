64e06594dee3ec5109a986cf576b254f3de8d7d2
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-25 12:55:44 +0530

    dashboard

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .dashboard
+    section.section
+      .container
+        .row
+          .col.s12
+            h3 Dashboard
+
+        .row
+          .col.s12
+            .btn Add field
+
+        .row
+          .col.s12
+            FieldOverview(field="1")
+          .col.s12
+            FieldOverview(field="2")
+
+</template>
+
+<script>
+// @ is an alias to /src
+import FieldOverview from '@/components/FieldOverview.vue'
+
+export default {
+	name: 'Dashboard',
+	components: {
+		FieldOverview
+	}
+}
+</script>
