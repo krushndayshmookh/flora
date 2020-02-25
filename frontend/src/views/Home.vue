3fd454dc6091ca1fabef0862f41e0e04d2af8ba9
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-25 12:55:56 +0530

    cleanup

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,28 +4,15 @@
       .container
         .row
           .col.s12
-            h3 Dashboard
+            h3 Home
 
-        .row
-          .col.s12
-            .btn Add field
-
-        .row
-          .col.s12
-            FieldOverview(field="1")
-          .col.s12
-            FieldOverview(field="2")
-
+        
 </template>
 
 <script>
 // @ is an alias to /src
-import FieldOverview from '@/components/FieldOverview.vue'
-
 export default {
 	name: 'Home',
-	components: {
-		FieldOverview
-	}
+	components: {}
 }
 </script>
