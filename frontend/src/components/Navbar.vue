3080218c33a187a709213eb49de5605889af45c4
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-25 15:15:06 +0530

    add dropdown for all views

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,16 +4,37 @@
         router-link(to="/").brand-logo Logo
         ul.right.hide-on-med-and-down
           li
-            router-link(to="/dashboard") Dashboard
+            router-link(to="/") Home
+          li
+            router-link(to="/fields") Fields
           li
             router-link(to="/market") Market
+          
           li
-            router-link(to="/field/0") Field
+            a.dropdown-trigger(href="#!" data-target="dropdown1") Pages
+              i.material-icons.right arrow_drop_down
+
+
+      ul#dropdown1.dropdown-content
+        li
+          router-link(to="/field/0") Field
+        li.divider
+        li
+          router-link(to="/register") Register
+
 </template>
 
 <script>
+import M from 'materialize-css'
 export default {
-	name: 'Navbar'
+	name: 'Navbar',
+	mounted() {
+		M.Dropdown.init(document.querySelector('.dropdown-trigger'), {
+			coverTrigger: false,
+			hover: true,
+			constrainWidth: false
+		})
+	}
 }
 </script>
 
