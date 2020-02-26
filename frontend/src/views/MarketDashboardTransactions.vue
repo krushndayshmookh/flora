71dc9aef0795443fe4c4078ed6c4d2229026e98e
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-26 16:10:29 +0530

    placeholder comps

---
+++ frontend/src/views/MarketDashboardTransactions.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .market
+    section.section
+      .container
+        .row
+          .col.s12
+            h3 Transactions
+
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+	name: 'MarketDashboardTransactions',
+	components: {},
+	data() {
+		return {}
+	},
+	methods: {}
+}
+</script>
