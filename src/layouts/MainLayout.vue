5129560f313bc97aadbe41524badfa78faf3aa87
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-28 13:42:30 +0530

    :art: cleanup main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,68 +3,28 @@
     q-header(elevated)
       q-toolbar
         q-btn(flat dense round icon="menu" aria-label="Menu" @click="leftDrawerOpen = !leftDrawerOpen")
-        q-toolbar-title Quasar App
+        q-toolbar-title Flora
         div Quasar v{{ $q.version }}
-    q-drawer(v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1")
-      q-list
-        q-item-label.text-grey-8(header) Essential Links
-        EssentialLink(v-for="link in essentialLinks" :key="link.title" v-bind="link")
+
+    LeftSideDrawer(:open="leftDrawerOpen")
     q-page-container
       router-view
 
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink'
+import LeftSideDrawer from 'components/LeftSideDrawer'
 
 export default {
   name: 'MainLayout',
 
   components: {
-    EssentialLink
+    LeftSideDrawer
   },
 
   data () {
     return {
       leftDrawerOpen: false,
-      essentialLinks: [
-        {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        }
-      ]
     }
   }
 }
