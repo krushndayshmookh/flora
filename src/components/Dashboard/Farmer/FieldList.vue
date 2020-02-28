f186cd07df0c0f2004296008ddf885b2e81ab67b
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-28 18:44:47 +0530

    :building_construction: pass data using props

--- src/components/Dashboard/Farmer/FieldList.vue
+++ src/components/Dashboard/Farmer/FieldList.vue
@@ -1,13 +1,7 @@
 <template lang="pug">
   .row
-    .col-12.col-sm-6.col-md-4.q-px-md.q-my-md
-      FieldCard
-    .col-12.col-sm-6.col-md-4.q-px-md.q-my-md
-      FieldCard
-    .col-12.col-sm-6.col-md-4.q-px-md.q-my-md
-      FieldCard
-    .col-12.col-sm-6.col-md-4.q-px-md.q-my-md
-      FieldCard
+    .col-12.col-sm-6.col-md-4.q-px-md.q-my-md(v-for="field in fields" :key="field.id")
+      FieldCard(:field="field")
 
 </template>
 
@@ -16,6 +10,7 @@ import FieldCard from 'components/Dashboard/Farmer/FieldCard'
 
 export default {
   name:'FieldList',
+  props:['fields'],
   components:{
     FieldCard
   }
--- src/pages/Dashboard/Farmer.vue
+++ src/pages/Dashboard/Farmer.vue
@@ -2,7 +2,7 @@
   q-page.q-pa-md
     .row
       h4.text-weight-light.q-px-md.q-my-sm Fields
-    FieldList
+    FieldList(:fields="fields")
 
 </template>
 
@@ -13,6 +13,42 @@ export default {
   name: 'DashboardFarmer',
   components: {
     FieldList
+  },
+  data() {
+    return {
+      fields: [
+        {
+          id: 0,
+          image: 'https://cdn.quasar.dev/img/parallax1.jpg',
+          title: 'Field 0',
+          crop: 'Field 0'
+        },
+        {
+          id: 1,
+          image: 'https://cdn.quasar.dev/img/parallax2.jpg',
+          title: 'Field 1',
+          crop: 'Field 1'
+        },
+        {
+          id: 2,
+          image: 'https://cdn.quasar.dev/img/parallax1.jpg',
+          title: 'Field 2',
+          crop: 'Field 2'
+        },
+        {
+          id: 3,
+          image: 'https://cdn.quasar.dev/img/parallax2.jpg',
+          title: 'Field 2',
+          crop: 'Field 2'
+        },
+        {
+          id: 4,
+          image: 'https://cdn.quasar.dev/img/parallax1.jpg',
+          title: 'Field 4',
+          crop: 'Field 4'
+        }
+      ]
+    }
   }
 }
 </script>
