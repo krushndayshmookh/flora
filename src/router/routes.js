1e09a43fbfc9279cb9f9359fd560388f2261819d
krushndayshmookh <krushndayshmookh@gmail.com>
2020-03-04 13:13:25 +0530

    :construction: add encylopedia routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,8 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: 'home', component: () => import('pages/Index.vue') },
+      { path: 'encyclopedia', component: () => import('pages/Encyclopedia/Index.vue') },
+      { path: 'encyclopedia/article/:id', component: () => import('pages/Encyclopedia/Article.vue') },
       { path: 'farmer', component: () => import('pages/Dashboard/Farmer.vue') },
       { path: 'farmer/field/:id', component: () => import('pages/Dashboard/Field.vue') },
       { path: 'about', component: () => import('pages/About.vue') }
