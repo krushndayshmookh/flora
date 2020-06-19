1eb6d32c410ccd43c0def1ff7ae8e78bd8cc863d
Dhobleanuprita <anupritadhoble@gmail.com>
2020-06-19 11:30:51 +0530

    Info

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="about">
-        <h1>About page</h1>
-        <p>About page content</p>
-        <p>About page another line</p>
+        <h1>About Flora</h1>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <p>Farmers face distress when weather or market situation is not favourable.We Plan to develop an e-portal for farmers for providing assistance for all their  agricultural needs. The farmers can directly communicate with the Doctors, Farmers, Traders and Experts for all the information they need. This portal will be monitored by government agencies, so  that farmers can register on the  portal  and government can automate, schedule and monitor farm activities. </p>
+        <p>Crop predictions  based on location, weather, state of farm can help them choose their crops. Also it  will provide assistance in buying and selling crops, seeds, tools, chemicals and getting loans.</p>
     </div>
 </template>
