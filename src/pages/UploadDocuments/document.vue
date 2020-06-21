72b7af1b22963f1330ae924a606aefea6367c6ba
anupritadhoble <anupritadhoble@gmail.com>
2020-06-21 18:58:33 +0530

    document formate added

---
+++ src/pages/UploadDocuments/document.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm row items-start">
+      <q-uploader
+        url="http://localhost:4444/upload"
+        label="Upload here"
+        multiple
+        style="max-width: 300px"
+      />
+    </div>
+  </div>
+</template>
