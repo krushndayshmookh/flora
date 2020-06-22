e067c1729a8cb40465780857640940108c93aaa1
anupritadhoble <anupritadhoble@gmail.com>
2020-06-22 20:21:49 +0530

    input field added

--- src/pages/UploadDocuments/document.vue
+++ src/pages/UploadDocuments/document.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="q-pa-md">
-    <div class="q-gutter-sm row items-start">
-      <q-uploader
-        url="http://localhost:4444/upload"
-        label="Upload here"
-        multiple
-        style="max-width: 300px"
-      />
+    <div class="q-gutter-y-md column" style="max-width: 300px">
+      <q-input label="Add Document name here!" rounded outlined v-model="text">
+        <template v-slot:append>
+          <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+        </template>
+      </q-input>
+      <div class="q-gutter-md row items-start">
+      <div class="q-pa-md">
+     <q-uploader
+     label="Upload"
+      url="http://localhost:4444/upload"
+      dark
+    />
+  </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
