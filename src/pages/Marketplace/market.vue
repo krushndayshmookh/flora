ce35579deb30067885ace8b06434fe7ade9c14cd
togarwarkirti <kirti.togarwar@gmail.com>
2020-07-17 11:49:23 +0530

    market added

---
+++ src/pages/Marketplace/market.vue
@@ -0,0 +1,103 @@
+  <template>
+  <div class="q-gutter-md q-pa-md">
+    
+    
+     <q-card class="my-card ">
+      <img src="https://cdn.quasar.dev/img/parallax1.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Tractors and Machines</div>
+        <!-- <div class="text-subtitle2">by John Doe</div> -->
+      </q-card-section>
+
+      <q-card-section side class="q-pt-none">
+       <q-btn v-ripple rounded color="primary" label="view" @click="toggleDialog(true)" />
+
+      </q-card-section>
+     </q-card>
+    
+     
+  
+     <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/parallax1.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Transport</div>
+        <!-- <div class="text-subtitle2">by John Doe</div> -->
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <q-btn v-ripple rounded color="primary" label="view" @click="toggleDialog(true)" />
+
+      </q-card-section>
+     </q-card>
+
+     
+  
+     <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/parallax1.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Tools and Chemicals</div>
+        <!-- <div class="text-subtitle2">by John Doe</div> -->
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <q-btn v-ripple rounded color="primary" label="view" @click="toggleDialog(true)" />
+
+      </q-card-section>
+     </q-card>
+  
+    
+  
+     <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/parallax1.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Seeds</div>
+        <!-- <div class="text-subtitle2">by John Doe</div> -->
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+         <!-- .q-px-md.q-my-md -->
+      <q-btn v-ripple rounded color="primary" label="view" @click="toggleDialog(true)" />
+
+      </q-card-section>
+     </q-card>
+  
+    
+
+
+  
+  </div>
+
+   
+
+
+
+</template>
+
+
+<script>
+export default {
+  // data () {
+  //   return {
+  //     lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  //   }
+  // }
+ methods: {
+    toggleDialog(val) {
+      this.fieldDialogOpen = val
+    }
+  }
+}
+
+</script>
+
+
+<style lang="sass" scoped>
+.my-card
+ 
+  width: 100%
+  max-width: 500px
+</style>
