e568c8632e8481cd0a6259e7bd05569354384e60
Kishor Baidya <kbaidya21@gmail.com>
2020-07-19 00:34:31 +0530

    new product page created

---
+++ src/pages/Dashboard/Product/Index.vue
@@ -0,0 +1,294 @@
+<template>
+<div class="products">
+    <div class="row">
+        <div class=" col-1 text-h6 register text-grey ">
+            Filters :
+        </div>
+        <div class="col-2">
+            <q-select bottom-slots v-model="model" :options="cat" label="" counter maxlength="12" :dense="dense" :options-dense="denseOpts" style="width:120px;margin-right:14px">
+                <template v-slot:hint>
+                    By Category
+                </template>
+            </q-select>
+        </div>
+        <div class="col-2">
+            <q-select bottom-slots v-model="model" :options="sel" label="" counter maxlength="12" :dense="dense" :options-dense="denseOpts" style="width:120px;margin-right:14px">
+                <template v-slot:hint>
+                    By Seller
+                </template>
+            </q-select>
+        </div>
+        <div class="col-2">
+            <q-select bottom-slots v-model="model" use-input use-chips multiple @new-value="createValue" :options="filterOptions" @filter="filterFn" style="width:120px;margin-right:14px">
+                <template v-slot:hint>
+                    Select Multiple Brand
+                </template>
+            </q-select>
+        </div>
+        <div class="col-3">
+            <q-input v-model="search" debounce="1000" placeholder="Search" hint="Debouncing 1000ms">
+                <template v-slot:append>
+                    <q-icon name="search" />
+                </template>
+            </q-input>
+        </div>
+    </div>
+    <div class="row">
+      <div class="col adjust-3">
+        <q-card>
+          <q-img src="~assets/images/fertilizer2.jpg" />
+
+          <q-card-section>
+            <q-btn
+              fab
+              color="primary"
+              icon="share"
+              class="absolute"
+              style="top: 0; right: 12px; transform: translateY(-50%);"
+            />
+
+            <div class="row no-wrap items-center">
+              <div class="col text-h6 ellipsis">
+                <a style="color:grey;text-decoration:none" :href="`/product`"> Agrovit - 500gm</a>
+              </div>
+              <div
+                class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
+              >
+                <q-icon name="place" />
+                Plant Fertilizer
+              </div>
+            </div>
+
+            <q-rating v-model="stars" :max="5" size="32px" />
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <div class="text-caption" secoundary>
+              In Stock (13pc) Price: ₹140.00
+            </div>
+            <div class="text-caption text-grey">
+              It is safe and an excellent wetting agent which improves<br />
+              wetting,spreading, sticking quality and enhance<br />
+              effectiveness of pesticidant spray.
+            </div>
+          </q-card-section>
+          <q-separator />
+          <q-card-actions>
+            <q-btn flat icon="shopping_cart" color="primary">
+              &nbsp; &nbsp;Add to Cart
+            </q-btn>
+            <q-btn flat icon="add_shopping_cart" color="primary">
+              &nbsp; &nbsp;Buy Now
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      
+      </div>
+      <div class="col adjust-3">
+        <q-card>
+          <q-img src="~assets/images/fertilizer2.jpg" />
+
+          <q-card-section>
+            <q-btn
+              fab
+              color="primary"
+              icon="share"
+              class="absolute"
+              style="top: 0; right: 12px; transform: translateY(-50%);"
+            />
+
+            <div class="row no-wrap items-center">
+              <div class="col text-h6 ellipsis">
+                <a style="color:grey;text-decoration:none" :href="`/product`"> Agrovit - 500gm</a>
+              </div>
+              <div
+                class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
+              >
+                <q-icon name="place" />
+                Plant Fertilizer
+              </div>
+            </div>
+
+            <q-rating v-model="stars" :max="5" size="32px" />
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <div class="text-caption" secoundary>
+              In Stock (13pc) Price: ₹140.00
+            </div>
+            <div class="text-caption text-grey">
+              It is safe and an excellent wetting agent which improves<br />
+              wetting,spreading, sticking quality and enhance<br />
+              effectiveness of pesticidant spray.
+            </div>
+          </q-card-section>
+          <q-separator />
+          <q-card-actions>
+            <q-btn flat icon="shopping_cart" color="primary">
+              &nbsp; &nbsp;Add to Cart
+            </q-btn>
+            <q-btn flat icon="add_shopping_cart" color="primary">
+              &nbsp; &nbsp;Buy Now
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      
+      </div><div class="col adjust-3">
+        <q-card>
+          <q-img src="~assets/images/fertilizer2.jpg" />
+
+          <q-card-section>
+            <q-btn
+              fab
+              color="primary"
+              icon="share"
+              class="absolute"
+              style="top: 0; right: 12px; transform: translateY(-50%);"
+            />
+
+            <div class="row no-wrap items-center">
+              <div class="col text-h6 ellipsis">
+                <div class="col text-h6 ellipsis">
+                <a style="color:grey;text-decoration:none" :href="`/product`"> Agrovit - 500gm</a>
+              </div>
+                
+                </div>
+              <div
+                class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
+              >
+                <q-icon name="place" />
+                Plant Fertilizer
+              </div>
+            </div>
+
+            <q-rating v-model="stars" :max="5" size="32px" />
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <div class="text-caption" secoundary>
+              In Stock (13pc) Price: ₹140.00
+            </div>
+            <div class="text-caption text-grey">
+              It is safe and an excellent wetting agent which improves<br />
+              wetting,spreading, sticking quality and enhance<br />
+              effectiveness of pesticidant spray.
+            </div>
+          </q-card-section>
+          <q-separator />
+          <q-card-actions>
+            <q-btn flat icon="shopping_cart" color="primary">
+              &nbsp; &nbsp;Add to Cart
+            </q-btn>
+            <q-btn flat icon="add_shopping_cart" color="primary">
+              &nbsp; &nbsp;Buy Now
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      
+      </div><div class="col adjust-3">
+        <q-card>
+          <q-img src="~assets/images/fertilizer2.jpg" />
+
+          <q-card-section>
+            <q-btn
+              fab
+              color="primary"
+              icon="share"
+              class="absolute"
+              style="top: 0; right: 12px; transform: translateY(-50%);"
+            />
+
+            <div class="row no-wrap items-center">
+              <div class="col text-h6 ellipsis">
+                <a style="color:grey;text-decoration:none" :href="`/product`"> Agrovit - 500gm</a>
+              </div>
+              <div
+                class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
+              >
+                <q-icon name="place" />
+                Plant Fertilizer
+              </div>
+            </div>
+
+            <q-rating v-model="stars" :max="5" size="32px" />
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <div class="text-caption" secoundary>
+              In Stock (13pc) Price: ₹140.00
+            </div>
+            <div class="text-caption text-grey">
+              It is safe and an excellent wetting agent which improves<br />
+              wetting,spreading, sticking quality and enhance<br />
+              effectiveness of pesticidant spray.
+            </div>
+          </q-card-section>
+          <q-separator />
+          <q-card-actions>
+            <q-btn flat icon="shopping_cart" color="primary">
+              &nbsp; &nbsp;Add to Cart
+            </q-btn>
+            <q-btn flat icon="add_shopping_cart" color="primary">
+              &nbsp; &nbsp;Buy Now
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      
+      </div>
+      
+    </div>
+    
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            model: null,
+            cat: [
+                'All', 'Cotton', 'Rice', 'Wheat', 'Corn', 'Fertilizer', 'seeds'
+            ],
+            sel: [
+                'All', 'Wheet', 'Corn', 'etc'
+            ],
+            filterOptions: [
+                'All', 'Wheet', 'Corn', 'etc'
+            ]
+
+        }
+    }
+}
+</script>
+
+<style>
+.register {
+    margin-top: 32px;
+    margin-bottom: 10px;
+    margin-left: 24px;
+    text-align: left;
+    width: 100px;
+    /* color:rgba(0, 0, 0, 0.54); */
+}
+
+.select-box {
+    /* margin-left: '35px'; */
+    width: 'auto';
+}
+.adjust-3{
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-top: 28px;
+  /* margin-bottom: 10px; */
+}
+.box-shadow {
+    box-shadow:
+        0 6.8px 8.2px rgba(0, 0, 0, 0.034),
+        0 9.7px 12.3px rgba(0, 0, 0, 0.048),
+        0 18.5px 23px rgba(0, 0, 0, 0.06),
+        0 32.3px 29.9px rgba(0, 0, 0, 0.072),
+        0 49.8px 54.4px rgba(0, 0, 0, 0.086),
+        0 120px 89px rgba(0, 0, 0, 0.12);
+
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,131 +1,172 @@
-const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', redirect: '/home' },
-      { path: 'home', component: () => import('pages/Index.vue') },
-      {
-        path: 'encyclopedia',
+const routes = [{
+        path: '/',
+        component: () =>
+            import ('layouts/MainLayout.vue'),
         children: [
-          {
-            path: '',
-            component: () => import('pages/Encyclopedia/Index.vue')
-          },
-          {
-            path: 'article/:id',
-            component: () => import('pages/Encyclopedia/Article.vue')
-          }
-        ],
-        component: () => import('layouts/Dashboard/Encyclopedia.vue')
-      },
-      {
-        path: 'farmer',
-        component: () => import('layouts/Dashboard/Farmer.vue'),
-        children: [
-          {
-            path: '',
-            component: () => import('pages/Dashboard/Farmer/Index.vue')
-          },
-          {
-            path: 'field/:id',
-            component: () => import('pages/Dashboard/Farmer/Field.vue')
-          }
-        ]
-      },
-      {
-        path: 'investor',
-        component: () => import('layouts/Dashboard/Investor.vue'),
-        children: [
-          {
-            path: '',
-            component: () => import('pages/Dashboard/Investor/Index.vue')
-          }
-        ]
-      },
-      {
-        path: 'consumer',
-        component: () => import('layouts/Dashboard/Consumer.vue'),
-        children: [
-          {
-            path: 'chemical/:id',
-            component: () => import('pages/Dashboard/Consumer/Chemical.vue')
-          },
-          {
-            path: '',
-            component: () => import('pages/Dashboard/Consumer/Index.vue')
-          }
-        ]
-      },
+            { path: '', redirect: '/home' },
+            {
+                path: 'home',
+                component: () =>
+                    import ('pages/Index.vue')
+            },
+            {
+                path: 'encyclopedia',
+                children: [{
+                        path: '',
+                        component: () =>
+                            import ('pages/Encyclopedia/Index.vue')
+                    },
+                    {
+                        path: 'article/:id',
+                        component: () =>
+                            import ('pages/Encyclopedia/Article.vue')
+                    }
+                ],
+                component: () =>
+                    import ('layouts/Dashboard/Encyclopedia.vue')
+            },
+            {
+                path: 'farmer',
+                component: () =>
+                    import ('layouts/Dashboard/Farmer.vue'),
+                children: [{
+                        path: '',
+                        component: () =>
+                            import ('pages/Dashboard/Farmer/Index.vue')
+                    },
+                    {
+                        path: 'field/:id',
+                        component: () =>
+                            import ('pages/Dashboard/Farmer/Field.vue')
+                    }
+                ]
+            },
+            {
+                path: 'investor',
+                component: () =>
+                    import ('layouts/Dashboard/Investor.vue'),
+                children: [{
+                    path: '',
+                    component: () =>
+                        import ('pages/Dashboard/Investor/Index.vue')
+                }]
+            },
+            {
+                path: 'consumer',
+                component: () =>
+                    import ('layouts/Dashboard/Consumer.vue'),
+                children: [{
+                        path: 'chemical/:id',
+                        component: () =>
+                            import ('pages/Dashboard/Consumer/Chemical.vue')
+                    },
+                    {
+                        path: '',
+                        component: () =>
+                            import ('pages/Dashboard/Consumer/Index.vue')
+                    }
+                ]
+            },
+            {
+                path: '/product',
+                component: () =>
+                    import ('layouts/Dashboard/Product.vue'),
+                children: [{
 
-      {
-        path: 'chatbox',
-        children: [
-          {
-            path: 'chat/:id',
-            component: () => import('pages/ChatBox/chat.vue')
-          },
-          {
-            path: 'contacts',
-            component: () => import('pages/ChatBox/contacts.vue')
-          }
-        ],
-        component: () => import('layouts/Chatbox.vue')
-      },
-      { path: 'retina', component: () => import('pages/Retina/Index.vue') },
+                    path: '',
+                    component: () =>
+                        import ('pages/Dashboard/Product/Index.vue')
+                }]
+            },
+            {
+                path: 'chatbox',
+                children: [{
+                        path: 'chat/:id',
+                        component: () =>
+                            import ('pages/ChatBox/chat.vue')
+                    },
+                    {
+                        path: 'contacts',
+                        component: () =>
+                            import ('pages/ChatBox/contacts.vue')
+                    }
+                ],
+                component: () =>
+                    import ('layouts/Chatbox.vue')
+            },
+            {
+                path: 'retina',
+                component: () =>
+                    import ('pages/Retina/Index.vue')
+            },
 
-      // { path: 'Advisory', component: () => import('pages/Advisory/Forum.vue') },
-      {
-        path: 'Advisory',
-        children: [
-          {
-            path: 'forum',
-            component: () => import('pages/Advisory/Forum.vue')
-          }
-          // {
-          //   path: 'contacts',
-          //   component: () => import('pages/ChatBox/contacts.vue')
-          // }
-        ],
-        component: () => import('layouts/Advisory.vue')
-      },
+            // { path: 'Advisory', component: () => import('pages/Advisory/Forum.vue') },
+            {
+                path: 'Advisory',
+                children: [{
+                        path: 'forum',
+                        component: () =>
+                            import ('pages/Advisory/Forum.vue')
+                    }
+                    // {
+                    //   path: 'contacts',
+                    //   component: () => import('pages/ChatBox/contacts.vue')
+                    // }
+                ],
+                component: () =>
+                    import ('layouts/Advisory.vue')
+            },
 
-      {
-        path: 'fertilopedia',
-        component: () => import('pages/Fertilopedia/Index.vue')
-      },
-      { path: 'profile', component: () => import('pages/Profile.vue') },
-      { path: 'about', component: () => import('pages/About.vue') },
-      // { path: 'chatbox', component: () => import('pages/ChatBox/chat.vue') },
-      // { path: 'contacts', component: () => import('pages/ChatBox/contacts.vue') }
-      {
-        path: 'uploaddocument',
-        component: () => import('pages/UploadDocuments/document.vue')
-      }
-    ]
-  },
-  {
-    path: '/auth',
-    component: () => import('layouts/AuthLayout.vue'),
-    children: [
-      {
-        path: 'login',
-        component: () => import('pages/Auth/Login.vue')
-      },
-      {
-        path: 'register',
-        component: () => import('pages/Auth/Register.vue')
-      }
-    ]
-  }
+            {
+                path: 'fertilopedia',
+                component: () =>
+                    import ('pages/Fertilopedia/Index.vue')
+            },
+            {
+                path: 'profile',
+                component: () =>
+                    import ('pages/Profile.vue')
+            },
+            {
+                path: 'about',
+                component: () =>
+                    import ('pages/About.vue')
+            },
+            // { path: 'chatbox', component: () => import('pages/ChatBox/chat.vue') },
+            // { path: 'contacts', component: () => import('pages/ChatBox/contacts.vue') }
+            {
+                path: 'uploaddocument',
+                component: () =>
+                    import ('pages/UploadDocuments/document.vue')
+            }
+        ]
+    },
+
+    {
+        path: '/auth',
+        component: () =>
+            import ('layouts/AuthLayout.vue'),
+        children: [{
+                path: 'login',
+                component: () =>
+                    import ('pages/Auth/Login.vue')
+            },
+            {
+                path: 'register',
+                component: () =>
+                    import ('pages/Auth/Register.vue')
+            }
+        ]
+    }
 ]
 
 // Always leave this as last one
 if (process.env.MODE !== 'ssr') {
-  routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
+    routes.push({
+        path: '*',
+        component: () =>
+            import ('pages/Error404.vue')
+    })
 }
 
 export default routes
