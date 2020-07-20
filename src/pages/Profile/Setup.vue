e018ae67db91f616e68d51c18741d127e723c230
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-07-20 10:10:38 +0530

    design profile setup page 1: select user type

--- src/pages/Profile/Setup.vue
+++ src/pages/Profile/Setup.vue
@@ -1,5 +1,112 @@
 <template>
   <q-page padding>
-    <div class="text-h1">Setup profile</div>
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">I am a ...</div>
+      </q-card-section>
+      <q-card-section>
+        <div class="row q-col-gutter-md">
+          <!-- <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/plasticine/400/000000/field.png"
+              />
+              Farmer
+            </q-btn>
+          </div> -->
+          <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img src="https://img.icons8.com/color/400/000000/field.png" />
+              Farmer
+            </q-btn>
+          </div>
+
+          <!-- <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/plasticine/400/000000/shipped.png"
+              />
+              Trader
+            </q-btn>
+          </div> -->
+          <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/color/400/000000/shipped.png"
+              />
+              Trader
+            </q-btn>
+          </div>
+
+          <!-- <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/plasticine/400/000000/doctor-female.png"
+              />
+              Doctor
+            </q-btn>
+          </div> -->
+          <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/color/400/000000/doctor-female.png"
+              />
+              Doctor
+            </q-btn>
+          </div>
+
+          <!-- <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/plasticine/400/000000/shopping-cart.png"
+              />
+              Comsumer
+            </q-btn>
+          </div> -->
+          <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/color/400/000000/shopping-cart.png"
+              />
+              Comsumer
+            </q-btn>
+          </div>
+
+          <!-- <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/plasticine/400/000000/literature.png"
+              />
+              Expert
+            </q-btn>
+          </div> -->
+          <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/color/400/000000/literature.png"
+              />
+              Expert
+            </q-btn>
+          </div>
+
+          <!-- <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/plasticine/400/000000/administrative-tools.png"
+              />
+              Admin
+            </q-btn>
+          </div> -->
+          <div class="col-6">
+            <q-btn color="primary" class="fit" outline stack>
+              <q-img
+                src="https://img.icons8.com/color/400/000000/administrative-tools.png"
+              />
+              Admin
+            </q-btn>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
