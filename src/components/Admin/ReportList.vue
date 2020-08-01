74062b8795a60a3a658214a9c3faa7c6d8226297
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-08-01 10:47:00 +0530

    create report list comp

---
+++ src/components/Admin/ReportList.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-list>
+    <q-item
+      v-for="report in reports"
+      :key="report._id"
+      v-ripple
+      clickable
+      :to="'/reports/' + report._id"
+    >
+      <q-item-section avatar>
+        <q-avatar>
+          <q-icon
+            size="32px"
+            :name="report.resolved ? 'check_circle' : 'report'"
+            :color="report.resolved ? 'green' : 'red'"
+          />
+        </q-avatar>
+      </q-item-section>
+      <q-item-section>
+        <div class="text-overline">{{ report.date }}</div>
+        <div class="text-body1">{{ report.title }}</div>
+        <div class="text-caption">{{ report.reporter.name }}</div>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+export default {
+  name: 'ReportList',
+
+  props: {
+    reports: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
