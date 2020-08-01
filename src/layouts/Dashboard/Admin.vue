cebf35aed0b21b38aa339422adcd0b32000bdae9
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-08-01 10:47:12 +0530

    create admin menu

---
+++ src/layouts/Dashboard/Admin.vue
@@ -0,0 +1,99 @@
+<template lang="pug">
+  router-view
+</template>
+
+<script>
+export default {
+  name: 'LayoutDashboardFarmer',
+
+  mounted() {
+    this.setLeftDrawerOptions()
+  },
+
+  methods: {
+    setLeftDrawerOptions() {
+      let options = [
+        {
+          link: '/admin',
+          icon: 'dashboard',
+          label: 'Home'
+        },
+        // {
+        //   link: '/farmer',
+        //   icon: 'spa',
+        //   label: 'Farm'
+        // },
+        // {
+        //   link: '/product',
+        //   icon: 'shopping_cart',
+        //   label: 'Products'
+        // },
+        // {
+        //   link: '/consumer',
+        //   icon: 'shopping_cart',
+        //   label: 'Services'
+        // },
+
+        // {
+        //   link: '/encyclopedia',
+        //   icon: 'book',
+        //   label: 'Encyclopedia'
+        // },
+        {
+          link: '/schemes',
+          icon: 'book',
+          label: 'Schemes'
+        },
+        // {
+        //   link: '/retina',
+        //   icon: 'visibility',
+        //   label: 'Retina'
+        // },
+        // {
+        //   link: '/chatbox/contacts',
+        //   icon: 'chat',
+        //   label: 'Forum'
+        // },
+        // {
+        //   link: '/fertilopedia',
+        //   icon: 'map',
+        //   label: 'Fertilopedia'
+        // },
+        // {
+        //   link: '/FertilizerCalci/Index',
+        //   icon: 'business',
+        //   label: 'FertilizerCalci'
+        // },
+        {
+          link: '/profile',
+          icon: 'account_circle',
+          label: 'Profile'
+        },
+        {
+          link: '/about',
+          icon: 'info',
+          label: 'About'
+        },
+        {
+          link: '/auth/login',
+          icon: 'lock',
+          label: 'Login'
+        },
+        {
+          link: '/auth/register',
+          icon: 'lock',
+          label: 'Register'
+        },
+        {
+          link: '/Advisory/Forum',
+          icon: 'help',
+          label: 'Advisory'
+        }
+      ]
+
+      this.$store.dispatch('general/setLeftSideDrawerOptions', options)
+      this.$store.dispatch('general/setCurrentUserType', 'admin')
+    }
+  }
+}
+</script>
