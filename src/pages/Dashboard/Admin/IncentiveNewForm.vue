92705607e648d827d967be3906f2dbc7a91db497
Kishor Baidya <kbaidya21@gmail.com>
2020-08-03 00:51:11 +0530

    fix issue

---
+++ src/pages/Dashboard/Admin/IncentiveNewForm.vue
@@ -0,0 +1,135 @@
+<template>
+  <q-page>
+    <div class="text-h6 q-pa-md">Create New Incentive</div>
+    <div class="row q-col-gutter-sm q-ma-xs">
+      <div class="col-lg-12 col-md-6 col-sm-12 col-xs-12">
+        <q-card>
+          <q-card-section>
+            <q-form class="q-gutter-md q-pa-sm">
+              <q-select
+                v-model="user.crop"
+                filled
+                label="Select Crop"
+                map-options
+                emit-value
+                :options="IncentiveSelectCrop"
+                class="q-mb-md"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="eco" />
+                </template>
+              </q-select>
+
+              <q-select
+                v-model="user.region"
+                filled
+                label="Select Region"
+                map-options
+                emit-value
+                :options="IncentiveRegion"
+                class="q-mb-md"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="map" />
+                </template>
+              </q-select>
+
+              <q-input
+                v-model="user.title"
+                filled
+                label="Title"
+                type="text"
+                class="q-mb-md"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="info" />
+                </template>
+              </q-input>
+
+              <q-input v-model="text" filled type="textarea">
+                <template v-slot:prepend>
+                  <q-icon name="tag" />
+                </template>
+              </q-input>
+
+              <div>
+                <q-btn  label="Send " type="submit" color="secondary" />
+              </div>
+            </q-form>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        color: 'cyan',
+
+        crop: 'Wheat',
+        farmer: '1',
+        email: 'k******@****.com',
+        phone: '76******52',
+        aadhar: '*********123',
+        exprience: '6',
+        address: 'Nagpur'
+      },
+
+      IncentiveSelectCrop: [
+        {
+          value: 'Wheat',
+          label: 'Wheat'
+        },
+        {
+          value: 'corn',
+          label: 'Corn'
+        },
+        {
+          value: 'rice',
+          label: 'Rice'
+        },
+        {
+          value: 'dal',
+          label: 'Dal'
+        },
+        {
+          value: 'cotton',
+          label: 'Cotton'
+        },
+        {
+          value: 'onions',
+          label: 'Onions'
+        }
+      ],
+      IncentiveRegion: [
+        {
+          value: 'nagpur',
+          label: 'Nagpur'
+        },
+        {
+          value: 'pune',
+          label: 'Pune'
+        },
+        {
+          value: 'amravati',
+          label: 'Amravati'
+        },
+        {
+          value: 'chandrapur',
+          label: 'Chandrapur'
+        },
+        {
+          value: 'akola',
+          label: 'Akola'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/pages/Dashboard/Admin/IncentiveNewNotice.vue
@@ -0,0 +1,169 @@
+<template>
+<div>
+    <div class="text-h6 q-pa-md">Incentive Details</div>
+  <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 q-pa-md">
+    <q-card class="my-card" bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">Nagpur Region</div>
+          <div class="text-h5 q-mt-sm q-pa-xs">
+            Crop : Rice
+          </div>
+          
+          <div class="text-caption text-grey q-pa-md">
+            Incentives to agricultural production are continuously changing in
+            response to shifts in both agricultural policies and agricultural
+            prices. These shifts impact the production and consumption choices
+            made by farmers and consumers, which in turn impact hunger and
+            malnutrition, change labor markets, and shape the global picture on
+            climate change. Historically, updated information and data on
+            agricultural incentives has been widely scattered and largely
+            inaccessible on a global scale. The Ag Incentives Consortium aims to
+            change this by bringing together key international organizations to
+            provide comparable incentives data in a single location.
+          </div>
+        </q-card-section>
+
+        <q-card-section class="col-3 flex flex-end ">
+          <q-img
+            class="rounded-borders"
+            src="images/bg_card_login.jpg"
+          />
+        </q-card-section>
+      </q-card-section>
+
+      <q-card-section> </q-card-section>
+    </q-card>
+  </div>
+
+  <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 q-pa-md">
+    <q-card class="my-card" bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">Nagpur Region</div>
+          <div class="text-h5 q-mt-sm q-pa-xs">
+            Crop : Rice
+          </div>
+          
+          <div class="text-caption text-grey q-pa-md">
+            Incentives to agricultural production are continuously changing in
+            response to shifts in both agricultural policies and agricultural
+            prices. These shifts impact the production and consumption choices
+            made by farmers and consumers, which in turn impact hunger and
+            malnutrition, change labor markets, and shape the global picture on
+            climate change. Historically, updated information and data on
+            agricultural incentives has been widely scattered and largely
+            inaccessible on a global scale. The Ag Incentives Consortium aims to
+            change this by bringing together key international organizations to
+            provide comparable incentives data in a single location.
+          </div>
+        </q-card-section>
+
+        <q-card-section class="col-3 flex flex-end ">
+          <q-img
+            class="rounded-borders"
+            src="images/bg_card_login.jpg"
+          />
+        </q-card-section>
+      </q-card-section>
+
+      <q-card-section> </q-card-section>
+    </q-card>
+  </div>
+
+  <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 q-pa-md">
+    <q-card class="my-card" bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">Nagpur Region</div>
+          <div class="text-h5 q-mt-sm q-pa-xs">
+            Crop : Rice
+          </div>
+          
+          <div class="text-caption text-grey q-pa-md">
+            Incentives to agricultural production are continuously changing in
+            response to shifts in both agricultural policies and agricultural
+            prices. These shifts impact the production and consumption choices
+            made by farmers and consumers, which in turn impact hunger and
+            malnutrition, change labor markets, and shape the global picture on
+            climate change. Historically, updated information and data on
+            agricultural incentives has been widely scattered and largely
+            inaccessible on a global scale. The Ag Incentives Consortium aims to
+            change this by bringing together key international organizations to
+            provide comparable incentives data in a single location.
+          </div>
+        </q-card-section>
+
+        <q-card-section class="col-3 flex flex-end ">
+          <q-img
+            class="rounded-borders"
+            src="images/bg_card_login.jpg"
+          />
+        </q-card-section>
+      </q-card-section>
+
+      <q-card-section> </q-card-section>
+    </q-card>
+  </div>
+
+  <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 q-pa-md">
+    <q-card class="my-card" bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">Nagpur Region</div>
+          <div class="text-h5 q-mt-sm q-pa-xs">
+            Crop : Rice
+          </div>
+          
+          <div class="text-caption text-grey q-pa-md">
+            Incentives to agricultural production are continuously changing in
+            response to shifts in both agricultural policies and agricultural
+            prices. These shifts impact the production and consumption choices
+            made by farmers and consumers, which in turn impact hunger and
+            malnutrition, change labor markets, and shape the global picture on
+            climate change. Historically, updated information and data on
+            agricultural incentives has been widely scattered and largely
+            inaccessible on a global scale. The Ag Incentives Consortium aims to
+            change this by bringing together key international organizations to
+            provide comparable incentives data in a single location.
+          </div>
+        </q-card-section>
+
+        <q-card-section class="col-3 flex flex-end ">
+          <q-img
+            class="rounded-borders"
+            src="images/bg_card_login.jpg"
+          />
+        </q-card-section>
+      </q-card-section>
+
+      <q-card-section> </q-card-section>
+    </q-card>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contacts: [
+        {
+          id: 1,
+          shop: 'Sonalika Tractor',
+          location: 'Nagpur',
+          name: 'Ruddy Jedrzej',
+          email: 'rjedrzej0@discuz.net',
+          number: '123-123-1234',
+          letter: 'S'
+        }
+      ]
+    }
+  },
+  mounted() {
+    this.fetchFarmers()
+  }
+}
+</script>
+
+<style></style>
