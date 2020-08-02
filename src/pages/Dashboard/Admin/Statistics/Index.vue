f37160d0c8b970512deb19b0e680ca2a4f32f24c
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-08-03 01:17:57 +0530

    move to stats ka index

---
+++ src/pages/Dashboard/Admin/Statistics/Index.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-page padding>
+    <div class="row q-col-gutter-md">
+      <div class="col-6 col-sm-4 col-md-3">
+        <q-btn
+          stack
+          label="Regional Crops"
+          class="fit q-pa-md"
+          icon="map"
+          to="/admin/statistics/region-crops"
+          color="green"
+        />
+      </div>
+
+      <div class="col-6 col-sm-4 col-md-3">
+        <q-btn
+          stack
+          label="Regional Crop Details"
+          class="fit q-pa-md"
+          icon="map"
+          to="/admin/statistics/region-crops-details"
+          color="blue"
+        />
+      </div>
+
+      <div class="col-6 col-sm-4 col-md-3">
+        <q-btn
+          stack
+          label="Regional Collection Centers"
+          class="fit q-pa-md"
+          icon="map"
+          to="/admin/statistics/region-centers"
+          color="orange"
+        />
+      </div>
+
+      <div class="col-6 col-sm-4 col-md-3">
+        <q-btn
+          stack
+          label="Regional Collection Center Details "
+          class="fit q-pa-md"
+          icon="map"
+          to="/admin/statistics/region-centers-details"
+          color="pink"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageStatistics'
+}
+</script>
