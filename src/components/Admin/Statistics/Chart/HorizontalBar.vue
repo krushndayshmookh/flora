e56c653b117a09bef5d277177fc1eb30d186d971
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-08-03 02:37:02 +0530

    create hosrixontal bar chart

---
+++ src/components/Admin/Statistics/Chart/HorizontalBar.vue
@@ -0,0 +1,25 @@
+<script>
+import { HorizontalBar, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+  extends: HorizontalBar,
+
+  mixins: [reactiveProp],
+
+  props: {
+    chartData: {
+      type: Object,
+      default: () => ({})
+    },
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
