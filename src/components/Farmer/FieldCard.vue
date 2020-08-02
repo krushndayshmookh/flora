3a59434dc902d554b719e0f0af5b3a11704e38bf
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-08-02 14:10:55 +0530

    updat field card lyaout adn random image selector. muhahahah. this is magic

--- src/components/Farmer/FieldCard.vue
+++ src/components/Farmer/FieldCard.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
   q-card
-    q-img(:src="field.image" style="height: 150px;")
-    q-card-section(horizontal)
-      q-card-section
-        .text-h6 {{ field.title }}
-        .text-subtitle2 {{ field.crop }}
-      q-card-section.absolute-right
-        q-btn(v-ripple outline  :to="'/farmer/field/' + field.id") View
+    q-img(:src="field.image || setImage()" style="height: 150px;")
+
+    q-card-section
+      .text-h6 {{ field.title }}
+      .text-subtitle2(v-if="field.currentlyOccupied") {{ field.crop.plant.name || 'Unoccupied' }}
+      .text-subtitle2(v-else) Unoccupied
+    q-card-actions
+      q-btn.full-width(v-ripple outline  :to="'/farmer/field/' + field.id") View
 </template>
 
 <script>
@@ -21,6 +22,18 @@ export default {
         title: '',
         crop: ''
       })
+    }
+  },
+
+  methods: {
+    setImage() {
+      const images = [
+        '/images/banner-1.jpg',
+        '/images/banner-2.jpg',
+        '/images/baner-3.png'
+      ]
+      let img = Math.floor((Math.random() * 3) % 3)
+      return images[img]
     }
   }
 }
