03c7ccc680d62c4a34735cedd65b8a901205aaed
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-08-03 01:18:54 +0530

    create doughnut chart comp

---
+++ src/components/Admin/Statistics/Chart/Doughnut.vue
@@ -0,0 +1,25 @@
+<script>
+import { Doughnut, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+  extends: Doughnut,
+
+  mixins: [reactiveProp],
+
+  props: {
+    chartData: {
+      type: Object,
+      default: () => ({})
+    },
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
