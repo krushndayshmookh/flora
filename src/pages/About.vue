c18f6c1a1a6797edb0b70566abee355006f2bcd7
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-08-02 23:47:07 +0530

    cleanup about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,37 +1,7 @@
 <template>
   <q-page class="q-pa-md">
-    <div class="q-pt-lg row ">
-      <div class="col-12 col-sm-4 col-md-4 q-pa-lg">
-        <q-card bordered>
-          <q-card-section style="text-align:center">
-            <img
-              src="https://cdn.iconscout.com/icon/free/png-512/avatar-380-456332.png"
-              style="width:100px; height 100px"
-            />
-            <q-card-section>
-              <div class="text-h6">Krushn Dayshmookh</div>
-              <p>krushndayshmookh@gmail.com <br />1234567890</p>
-            </q-card-section>
-          </q-card-section>
-        </q-card>
-      </div>
-
-      <div class="col-12 col-sm-4 col-md-4 q-pa-lg">
-        <q-card bordered>
-          <q-card-section style="text-align:center">
-            <img
-              src="https://cdn.iconscout.com/icon/free/png-512/avatar-380-456332.png"
-              style="width:100px; height 100px"
-            />
-            <q-card-section>
-              <div class="text-h6">Kishor Baidya</div>
-              <p>kishorbaidya@gmail.com <br />1234567890</p>
-            </q-card-section>
-          </q-card-section>
-        </q-card>
-      </div>
-
-      <div class="col-12 col-sm-4 col-md-4 q-pa-lg">
+    <div class="q-col-gutter-lg row ">
+      <div class="col-12 col-sm-6 col-md-4">
         <q-card bordered>
           <q-card-section style="text-align:center">
             <img
@@ -40,15 +10,13 @@
             />
             <q-card-section>
               <div class="text-h6">Vithika Jha</div>
-              <p>vithikajha@gmail.com <br />1234567890</p>
+              <p>vithikajha14@gmail.com <br />+91 7020 441 930</p>
             </q-card-section>
           </q-card-section>
         </q-card>
       </div>
-    </div>
 
-    <div class="q-pt-lg row ">
-      <div class="col-12 col-sm-4 col-md-4 q-pa-lg">
+      <div class="col-12 col-sm-6 col-md-4">
         <q-card bordered>
           <q-card-section style="text-align:center">
             <img
@@ -56,14 +24,29 @@
               style="width:100px; height 100px"
             />
             <q-card-section>
-              <div class="text-h6">Kirti Togarwar</div>
-              <p>kirtitogarwar@gmail.com <br />1234567890</p>
+              <div class="text-h6">Krushn Dayshmookh</div>
+              <p>krushndayshmookh@gmail.com <br />+91 776 706 0939</p>
             </q-card-section>
           </q-card-section>
         </q-card>
       </div>
 
-      <div class="col-12 col-sm-4 col-md-4 q-pa-lg">
+      <div class="col-12 col-sm-6 col-md-4">
+        <q-card bordered>
+          <q-card-section style="text-align:center">
+            <img
+              src="https://cdn.iconscout.com/icon/free/png-512/avatar-380-456332.png"
+              style="width:100px; height 100px"
+            />
+            <q-card-section>
+              <div class="text-h6">Kishor Baidya</div>
+              <p>kishorbaidya@gmail.com <br />+91 776 706 0939</p>
+            </q-card-section>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-12 col-sm-6 col-md-4">
         <q-card bordered>
           <q-card-section style="text-align:center">
             <img
@@ -72,13 +55,28 @@
             />
             <q-card-section>
               <div class="text-h6">Anuprita Dhoble</div>
-              <p>anupritadhoble@gmail.com <br />1234567890</p>
+              <p>anupritadhoble@gmail.com <br />+91 776 706 0939</p>
             </q-card-section>
           </q-card-section>
         </q-card>
       </div>
 
-      <div class="col-12 col-sm-4 col-md-4 q-pa-lg">
+      <div class="col-12 col-sm-6 col-md-4">
+        <q-card bordered>
+          <q-card-section style="text-align:center">
+            <img
+              src="https://cdn.iconscout.com/icon/free/png-512/avatar-380-456332.png"
+              style="width:100px; height 100px"
+            />
+            <q-card-section>
+              <div class="text-h6">Kirti Togarwar</div>
+              <p>kirtitogarwar@gmail.com <br />+91 776 706 0939</p>
+            </q-card-section>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <!-- <div class="col-12 col-sm-6 col-md-4">
         <q-card bordered>
           <q-card-section style="text-align:center">
             <img
@@ -87,18 +85,11 @@
             />
             <q-card-section>
               <div class="text-h6">Kaushal Agnihotri</div>
-              <p>kaushalagnihotri1997@gmail.com <br />1234567890</p>
+              <p>kaushalagnihotri1997@gmail.com <br />+91 776 706 0939</p>
             </q-card-section>
           </q-card-section>
         </q-card>
-      </div>
+      </div> -->
     </div>
   </q-page>
 </template>
-
-<style lang="sass" scoped></style>
-
-<style lang="sass" scoped>
-.my-card
-  width: auto
-</style>
