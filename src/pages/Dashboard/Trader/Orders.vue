02077cea675bc6e8c4c28bed94c10d950efba78a
Kishor Baidya <kbaidya21@gmail.com>
2020-08-03 12:55:44 +0530

    order list for trader

---
+++ src/pages/Dashboard/Trader/Orders.vue
@@ -0,0 +1,184 @@
+<template>
+  <q-page>
+    <div class="text-h5 q-pa-md">Order Statistics</div>
+
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Order By : Kishor Baidya
+          </div>
+          <div class="text-caption">Mob No : 7620053352</div>
+          <div class="text-caption">Dilevery : Nagpur</div>
+          <div class="text-caption">Pincode : 440016</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Tracking Id : 123456789</div>
+          <div class="text-overline">Payment : Cash On Dilevery</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer1.jpg" />
+          </q-avatar>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Order By : Krushn Dayshmookh
+          </div>
+          <div class="text-caption">Mob No : 7620053352</div>
+          <div class="text-caption">Dilevery : Nagpur</div>
+          <div class="text-caption">Pincode : 440016</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Tracking Id : 123456789</div>
+          <div class="text-overline">Payment : UPI</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer2.jpg" />
+          </q-avatar>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Order By : Vithika Jha
+          </div>
+          <div class="text-caption">Mob No : 7620053352</div>
+          <div class="text-caption">Dilevery : Nagpur</div>
+          <div class="text-caption">Pincode : 440016</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Tracking Id : 123456789</div>
+          <div class="text-overline">Payment : Cash On Dilevery</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer3.jpg" />
+          </q-avatar>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Order By : Anuprita 
+          </div>
+          <div class="text-caption">Mob No : 7620053352</div>
+          <div class="text-caption">Dilevery : Nagpur</div>
+          <div class="text-caption">Pincode : 440016</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Tracking Id : 123456789</div>
+          <div class="text-overline">Payment : UPI</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer1.jpg" />
+          </q-avatar>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Order By : Kirti
+          </div>
+          <div class="text-caption">Mob No : 7620053352</div>
+          <div class="text-caption">Dilevery : Nagpur</div>
+          <div class="text-caption">Pincode : 440016</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Tracking Id : 123456789</div>
+          <div class="text-overline">Payment : Cash On Dilevery</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer4.jpg" />
+          </q-avatar>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Order By : Kaustubh Dongare
+          </div>
+          <div class="text-caption">Mob No : 7620053352</div>
+          <div class="text-caption">Dilevery : Nagpur</div>
+          <div class="text-caption">Pincode : 440016</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Tracking Id : 123456789</div>
+          <div class="text-overline">Payment : UPI</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer5.jpg" />
+          </q-avatar>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Order By : Rahul Haldar
+          </div>
+          <div class="text-caption">Mob No : 7620053352</div>
+          <div class="text-caption">Dilevery : Nagpur</div>
+          <div class="text-caption">Pincode : 440016</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Tracking Id : 123456789</div>
+          <div class="text-overline">Payment : Cash On Dilevery</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer6.jpg" />
+          </q-avatar>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Order By : Tushar Joshi
+          </div>
+          <div class="text-caption">Mob No : 7620053352</div>
+          <div class="text-caption">Dilevery : Nagpur</div>
+          <div class="text-caption">Pincode : 440016</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Tracking Id : 123456789</div>
+          <div class="text-overline">Payment : UPI</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer3.jpg" />
+          </q-avatar>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+
+  data() {
+    return {}
+  }
+}
+</script>
