26208540b4e09229982f0d99d18e880fb434293d
Kishor Baidya <kbaidya21@gmail.com>
2020-08-03 13:36:50 +0530

    manage product and services

---
+++ src/pages/Dashboard/Trader/Products.vue
@@ -0,0 +1,192 @@
+<template>
+  <q-page>
+    <div class="text-h5 q-pa-md">Manage Product</div>
+
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Product Name : Agrovit - 500gm
+          </div>
+          <div class="text-caption">In Stock (13pc) Price: ₹140.00</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Fertilizer</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer1.jpg" />
+          </q-avatar>
+        </q-item-section>
+        <q-card-actions align="right" class="q-pa-sm" style="margin-left:12px">
+          <q-btn label="EDIT " type="submit" color="primary" />
+          <q-btn label="DELETE " type="submit" color="negative" />
+        </q-card-actions>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Product Name : Humacid - 500ml
+          </div>
+          <div class="text-caption">In Stock (13pc) Price: ₹360.00</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Fertilizer</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer2.jpg" />
+          </q-avatar>
+        </q-item-section>
+        <q-card-actions align="right" class="q-pa-sm" style="margin-left:12px">
+          <q-btn label="EDIT " type="submit" color="primary" />
+          <q-btn label="DELETE " type="submit" color="negative" />
+        </q-card-actions>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Product Name : MahaFlora - 1 Kg
+          </div>
+          <div class="text-caption">In Stock (13pc) Price: ₹360.00</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Fertilizer</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer3.jpg" />
+          </q-avatar>
+        </q-item-section>
+        <q-card-actions align="right" class="q-pa-sm" style="margin-left:12px">
+          <q-btn label="EDIT " type="submit" color="primary" />
+          <q-btn label="DELETE " type="submit" color="negative" />
+        </q-card-actions>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Product Name : MahaGreen - 1 Kg
+          </div>
+          <div class="text-caption">In Stock (13pc) Price: ₹360.00</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Fertilizer</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer1.jpg" />
+          </q-avatar>
+        </q-item-section>
+        <q-card-actions align="right" class="q-pa-sm" style="margin-left:12px">
+          <q-btn label="EDIT " type="submit" color="primary" />
+          <q-btn label="DELETE " type="submit" color="negative" />
+        </q-card-actions>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Product Name : Cow Manure - 5 Kg
+          </div>
+          <div class="text-caption">In Stock (13pc) Price: ₹360.00</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Fertilizer</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer4.jpg" />
+          </q-avatar>
+        </q-item-section>
+        <q-card-actions align="right" class="q-pa-sm" style="margin-left:12px">
+          <q-btn label="EDIT " type="submit" color="primary" />
+          <q-btn label="DELETE " type="submit" color="negative" />
+        </q-card-actions>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Product Name : Vermicompost - 5 kg
+          </div>
+          <div class="text-caption">In Stock (13pc) Price: ₹360.00</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Fertilizer</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer5.jpg" />
+          </q-avatar>
+        </q-item-section>
+        <q-card-actions align="right" class="q-pa-sm" style="margin-left:12px">
+          <q-btn label="EDIT " type="submit" color="primary" />
+          <q-btn label="DELETE " type="submit" color="negative" />
+        </q-card-actions>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Product Name : MahaFlora - 1 Kg
+          </div>
+          <div class="text-caption">In Stock (13pc) Price: ₹360.00</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Fertilizer</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer6.jpg" />
+          </q-avatar>
+        </q-item-section>
+        <q-card-actions align="right" class="q-pa-sm" style="margin-left:12px">
+          <q-btn label="EDIT " type="submit" color="primary" />
+          <q-btn label="DELETE " type="submit" color="negative" />
+        </q-card-actions>
+      </q-item>
+    </q-list>
+    <q-list bordered padding class="q-pa-md">
+      <q-item v-ripple :to="'#'" clickable>
+        <q-item-section>
+          <div class="text-subtitle text-bold">
+            Product Name : MahaGreen - 1 Kg
+          </div>
+          <div class="text-caption">In Stock (13pc) Price: ₹360.00</div>
+        </q-item-section>
+        <q-item-section>
+          <div class="text-overline">Fertilizer</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-avatar size="64px">
+            <q-img class="rounded-borders" src="images/fertilizer3.jpg" />
+          </q-avatar>
+        </q-item-section>
+        <q-card-actions align="right" class="q-pa-sm" style="margin-left:12px">
+          <q-btn label="EDIT " type="submit" color="primary" />
+          <q-btn label="DELETE " type="submit" color="negative" />
+        </q-card-actions>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+
+  data() {
+    return {}
+  }
+}
+</script>
